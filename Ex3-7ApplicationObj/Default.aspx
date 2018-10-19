f7b1ca63707d6becd0adef38515789d4b6f5996f
469_100006940582_business122 <469_100006940582_business122@git.cloud.tencent.com>
2018-10-19 17:02:15 +0800

    新增Application对象测试用例

---
+++ Ex3-7ApplicationObj/Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Ex3_7ApplicationObj.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+     <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label1" runat="server" Text="用户名:"></asp:Label>
+        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <asp:CheckBox ID="CheckBox1" runat="server" Text="记住我" />
+    
+    </div>
+        <asp:Label ID="Label2" runat="server" Text="密  码:"></asp:Label>
+        <asp:TextBox ID="TextBox2" runat="server" style="margin-left: 9px"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" Text="登录" OnClick="Button1_Click" />
+    </form>
+</body>
+</html>
---
+++ Ex3-7ApplicationObj/Default.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ex3_7ApplicationObj
+{
+    public partial class Default : System.Web.UI.Page
+    {
+
+        private static readonly string[] users = new string[] { "admin", "user" };
+        private int usertype(string userid)
+        {
+            if (userid == users[0])
+                return 1;
+            if (userid == users[1])
+                return 2;
+            else
+                return 0;
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string userid = TextBox1.Text.ToString();
+            string pwd = TextBox2.Text.ToString();
+
+            if (usertype(userid) == 1
+                || usertype(userid) == 2)
+                Application.Lock();
+
+            switch( usertype(userid).ToString() )
+            {
+                case "1":
+                    Application["admin"] = Convert.ToInt32(Application["admin"]) + 1;
+                    break;
+                case "2":
+                    Application["user"] = Convert.ToInt32(Application["user"]) + 1;
+                    break;
+                default:
+                    Response.Write("<script>alert('对不起，你不是合法用户!')</script>");
+                    break; 
+            }
+
+            Application.UnLock();
+            Response.Redirect("Total.aspx?userid="+userid);
+
+
+        }
+    }
+}
